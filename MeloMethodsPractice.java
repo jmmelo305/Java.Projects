c4f0585751360c6ce2d47e60ab069f75f3fc1209
jmmelo305 <jmmelo.edu@gmail.com>
2025-09-16 13:53:52 +0000

    updated code

---
+++ MeloMethodsPractice.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class MeloMethodsPractice{
+
+
+
+    //public static double getMontlyPayment (double annualInterestRate, double numberOfYears, double loanAmount){
+        
+   // }
+
+    //public static double getTotalPayment(double annualInterestRate, double numberOfYears, double loanAmount){
+    public static String getTotalPayment(String output){
+        System.out.println(output);
+        return output;
+        }
+
+
+
+
+
+    public static void main(String [] args){
+        Scanner input = new Scanner (System.in);
+        String word = input.nextLine();
+
+        getTotalPayment(word);
+
+
+
+
+        
+
+
+    
+    }
+}
