3b603bdd8a7740997d10c809e4dce421e3cd50b5
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-02 02:18:56 +0000

    Updated Code 10:18pm 10/1/2025

--- MeloLargestElement.java
+++ MeloLargestElement.java
@@ -7,40 +7,50 @@
  * Date: 10/1/2025
  */
 
+
+//Import Scanner
 import java.util.Scanner;
-import java.util.Arrays;
 public class MeloLargestElement {
+
+    //Create Main Method
     public static void main (String [] args){
         
+        //Declare Variables and Arrays in Main Method.
         int rowNum;
         int colNum;
         double [] [] array;
         int [] location;
 
+
+        //Prompt User for input.
         System.out.println("Enter the number of rows and columns of the array: ");
         Scanner input = new Scanner(System.in);
         rowNum = input.nextInt();
         colNum = input.nextInt();
         
-        
+        //Apply Newly inputted data into the array
         array = new double [rowNum][colNum];
         
         System.out.println();
 
+        //Prompt User to input the information for the array.
         System.out.println("Enter the array: ");
         for (int i = 0; i < rowNum; i++){
             for (int j = 0; j < colNum; j++){
                 array[i][j] = input.nextDouble();
             }
         }
+
+        //Pull locateLargest Method and print results
         location = locateLargest (array);
+        System.out.println();
         System.out.println("The location of the largest element is at (" + location[0] + ", " + location[1] + ")");
         
         
         input.close();      
     }    
             
-            
+    //locateLargest Method: Used to find what number is the largest in the array and where it is located.      
     public static int [] locateLargest (double [] [] a){
         int [] location = new int [2];
         double element = a[0][0];      
