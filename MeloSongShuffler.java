a78de01970cecebe6cef2cc9d2bbdf4015b4a52b
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-03 12:58:39 +0000

    Song Shuffler Program Completed

--- MeloSongShuffler.java
+++ MeloSongShuffler.java
@@ -1,15 +1,40 @@
-import java.util.Random;
+/*
+Name: Melo
+Date: 10/2/2025
+Description:  Prints the shuffled playlist to the console.
+*/
 
 public class MeloSongShuffler {
-    public static void main (String [] args){
-        String [] setPlaylist = {"Shake It Off - Taylor Swift", "I love You - Barney", "Baby Shark - pinkfong", "Never Gonna Give You up - Rick Astley", "Axel F (Crazy Frog) - Crazy Frog", "Gangam Style - PSY", "What Does the Fox Say - Ylvis"}; 
+    public static void main(String[] args) {
+        String[] playlist = {
+        "Shake It Off – Taylor Swift",
+        "I Love You – Barney",
+        "Baby Shark – Pinkfong",
+        "Never Gonna Give You Up – Rick Astley",
+        "Axel F (Crazy Frog) – Crazy Frog",
+        "Gangnam Style – PSY",
+        "What Does the Fox Say? – Ylvis"};
 
-        shuffle
+        shuffle(playlist);
+
+        System.out.println("Shuffled Playlist");
+        System.out.println();
+        for (String song : playlist){
+            System.out.println(song);
+        }
 
     }
 
-    public static String [] shuffle(String [] playlist){
-        for (int j = 0; j < playlist.length; j++);
-        
-    }
+
+    public static String [] shuffle(String[] playlist) {
+        String [] shuffledPlaylist = new String[7];
+        int i = 0;
+        for (int j = playlist.length; j < 7; j--);
+            int j = (int)(Math.random() * (i + 1));
+            String temp = playlist [i];
+            playlist [i] = playlist [j];
+            playlist [j] = temp;
+            return shuffledPlaylist;
+        }
 }
+
