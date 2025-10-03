abda9c23390decd5c66718c9077460f9fc772690
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-03 12:22:14 +0000

    blah blah

---
+++ MeloSongShuffler.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class MeloSongShuffler {
+    public static void main (String [] args){
+        String [] setPlaylist = {"Shake It Off - Taylor Swift", "I love You - Barney", "Baby Shark - pinkfong", "Never Gonna Give You up - Rick Astley", "Axel F (Crazy Frog) - Crazy Frog", "Gangam Style - PSY", "What Does the Fox Say - Ylvis"}; 
+
+        shuffle
+
+    }
+
+    public static String [] shuffle(String [] playlist){
+        for (int j = 0; j < playlist.length; j++);
+        
+    }
+}
