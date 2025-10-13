53613dc494f3eb34d7525dce7d972d2f79788459
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-13 13:52:38 +0000

    Working on my own Library Management System

---
+++ MeloOwnProject.java
@@ -0,0 +1,28 @@
+/*
+ * PROGRAMMER: Melo
+ * PROGRAM: Library Management System - Ask for input about a book name and author, organize it into an array (maybe even 2d) and print out the library list 
+ * DATE: 10-09-2025
+ */
+
+
+import java.util.Scanner;
+public class MeloOwnProject {
+    public static void main (String []args){
+    
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter List of Books");
+    
+        //int length = .Length(input);
+        String [] bookList = new String [length];
+        String [] authorName;
+
+        for (int i = 0; i < length; i++;){
+            bookList[i] = input.nextLine();
+
+        }
+
+
+        input.close();
+    }
+}
+
