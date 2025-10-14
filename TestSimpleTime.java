8ad021315adfea3aa7fbeb2c8d16381a146eb63b
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-14 13:45:04 +0000

    TestSimpleTime

---
+++ TestSimpleTime.java
@@ -0,0 +1,34 @@
+
+
+//YOU GOTTA FIGURE OUT HOW TO DO THIS BY YOURSELF
+//RE-DO THE PROJECT WITHOUT LOOKING AT THIS CODE
+
+
+
+
+
+
+// SimpleTime class with public fields
+class SimpleTime {
+    public int hour;
+    public int minute;
+    public int second;
+}
+
+// Test class with main method
+public class TestSimpleTime {
+    public static void main(String[] args) {
+        // Create a SimpleTime object
+        SimpleTime time = new SimpleTime();
+
+        // Assign values to hour, minute, and second
+        time.hour = 2;
+        time.minute = 3;
+        time.second = 4;
+
+        // Print the values
+        System.out.println("Hour: " + time.hour);
+        System.out.println("Minute: " + time.minute);
+        System.out.println("Second: " + time.second);
+    }
+}
