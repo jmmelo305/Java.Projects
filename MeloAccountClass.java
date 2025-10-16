c38b0051d8ca88188fb59b7bc6b5d8201c68670e
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-16 02:16:26 +0000

    Finished version of MeloAccountClass

--- MeloAccountClass.java
+++ MeloAccountClass.java
@@ -5,52 +5,96 @@
  * Date: 10-15-2025
  */
 
+
+ //import java Date to use Date variable in program
 import java.util.Date;
-public class MeloAccountClass {
-  public static void main (String[] args) {
-    Account account = new Account(1122, 20000);
-    Account.setAnnualInterestRate(4.5);
-    
-    
-    account.withdraw(2500);
-    account.deposit(3000);
-    System.out.println("Balance is " + account.getBalance());
-    System.out.println("Monthly interest is " +
-      account.getMonthlyInterest());
-    System.out.println("This account was created at " +
-      account.getDateCreated());
-  }
+
+//Create main class
+public class MeloAccountClass{
+
+  //Create main method within main class that pulls all the data from other classes and methods
+    public static void main (String [] args){
+        Account account = new Account(1122, 20000);
+        Account.setAnnualInterestRate(4.5);
+        
+
+        account.withdraw(2500);
+        account.deposit(3000);
+        System.out.println("Balance is " + account.getBalance());
+        System.out.println("Monthly interest is " + 
+            account.getMonthlyInterest());
+        System.out.println("This account was created at " + 
+            account.getDateCreated());
+    }
 }
 
-class account {
+//Creates second class called Account
+class Account {
 
-  // Implement the class here
+  //Assigns variables and default values
   private int id = 0;
   private double balance = 0;
-  private double annualInterestRate = 0;
+  private static double annualInterestRate = 0; // percentage, e.g. 4.5
   private Date dateCreated;
 
- 
-
-  public static void defaultAccount (){
-
-  }
-  
-  public double getMonthlyInterest(){
-
-
+  //no-arg constructor
+  public Account() {
+    this(0, 0.0);
   }
 
-  public Date getDateCreated(){
-    
+  //Creates method Account within Account class and assigns local variables to each value
+  public Account(int id, double balance) {
+    this.id = id;
+    this.balance = balance;
+    this.dateCreated = new Date();
   }
-  public static double withdraw (double amount){
+  //Method to be able to use the private ID variable
+  public int getId() { 
+    return id; 
+  }
+  //Method to assign variable "ID"
+  public void setId(int id) { 
+    this.id = id; 
+  }
+  //getBalance method to be able to use the private balance variable
+  public double getBalance() {
+     return balance;
+  }
+  //Method to assign variable "balance"
+  public void setBalance(double balance) {
+     this.balance = balance; 
+  }
+  //Method used to be able to use the private annualinterestrate variable
+  public static double getAnnualInterestRate() {
+     return annualInterestRate; 
+    }
 
+  //Method to assign variable annualinterestrate
+  public static void setAnnualInterestRate(double rate) {
+     annualInterestRate = rate; 
+    }
+
+  //Method that uses annualinterestrate to calculate the monthlyinterest rate
+  public double getMonthlyInterest() {
+    return balance * (annualInterestRate / 100.0) / 12.0;
+  }
+  //Method that uses the import java.util.date to grab the date (although it is in UTC time zone)
+  public Date getDateCreated() {
+    return dateCreated;
   }
 
-  public static double deposit (double amount){
-
+  //Withdraw method is used to withdraw a specified amount from the account.
+  public void withdraw(double amount) {
+    if (amount <= 0) return;
+    if (amount > balance) {
+      System.out.println("Insufficient funds");
+      return;
+    }
+    balance -= amount;
   }
-
-
+  //Deposit method that deposists a specified amount to the account.
+  public void deposit(double amount) {
+    if (amount <= 0) return;
+    balance += amount;
+  }
 }
