c2a14969e50d6cf0849e51aba6d3570e8b1058b7
jmmelo305 <jmmelo.edu@gmail.com>
2025-10-21 13:35:57 +0000

    OOP Practice Java Program

---
+++ MeloOOPPractice.java
@@ -0,0 +1,20 @@
+/*
+ * Programmer: Jose Melo
+ * Purpose: 
+ * Program: OOP Practice 
+ * Date: 10-21-2025
+ */
+
+import java.math.*;
+import java.util.Scanner;
+public class MeloOOPPractice {
+    public static void main (String [] args){
+
+    }
+    
+    public static BigInteger getNumberOfCombinations (int n, int k){
+
+    }
+
+    
+}
